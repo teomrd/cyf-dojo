cc8e2baf44fd9005557d73ceafbcf84054d6dcb4
Thodoris Mironidis <mironidis.gr@gmail.com>
2021-10-24 13:00:07 +0100

    say hello to terminal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+// object
 const user = {
   name: 'Theo',
   age: 34  
 }
 
-const name = 'Sokratis';
+const name = 'Sokratis'; // string 
+
+function sayHello(name, age) {
+  const message = `Hi ${name}, age: ${age}`;
+  
+  return message;
+};
+
+const theoHello = sayHello(user.name, 67);
+const sokratisGreeting = sayHello(name, 33);
+
