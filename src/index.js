fd2162cf1f2cc05010873cd8d41ace632a4dbf42
Thodoris Mironidis <mironidis.gr@gmail.com>
2021-10-24 13:10:25 +0100

    homework

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 // object
-const user = {
+const user1 = {
   name: 'Theo',
   age: 34  
 }
+const user2 = {
+  name: 'John',
+  age: 43  
+}
+const user3 = {
+  name: 'Nick',
+  age: 12  
+}
 
-const name = 'Sokratis'; // string 
+const user4 = {
+  name: 'Jack',
+  age: 8  
+}
 
-function sayHello(name, age) {
-  const message = `Hi ${name}, age: ${age}`;
-  
-  return message;
-};
+const users = [
+  user1,
+  user2,
+  user3,
+  user4
+];
 
-const theoHello = sayHello(user.name, 67);
-const sokratisGreeting = sayHello(name, 33);
+
+// 1. average age of users?
+
+// 2. all adult users
+
+// 3. all adult users names
+
+// 4. all users but with their name in UPPERCASE, e.g. { name: 'JACK' }
+
+
+
+
 
